6771584ca46f755c3c1dc72532361604ef83df2e
Mark <mpietryga93@gmail.com>
2017-04-17 11:34:36 +0200

    Add gray image preview

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,11 @@ int main(int argc, char** argv)
 	string image_name = "img\\test.png";
 	cout << image_name << endl;
 
-	Mat image;
+	Mat image, gray_image;
 	image = imread(image_name, IMREAD_COLOR);
+	cvtColor(image, gray_image, CV_BGR2GRAY);
+	cout << image << endl << endl;
+	cout << gray_image << endl;
 
 	if (image.empty())
 	{
@@ -22,9 +25,14 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	namedWindow(image_name, CV_WINDOW_AUTOSIZE);
-	resize(image, image, Size(), 20, 20); //for better preview test.png
-	imshow(image_name, image);
+	string image_titles[] = { "Plain image", "Gray image" };
+	namedWindow(image_titles[0], CV_WINDOW_AUTOSIZE);
+	namedWindow(image_titles[1], CV_WINDOW_AUTOSIZE);
+	//for better preview test.png
+	resize(image, image, Size(), 20, 20);
+	resize(gray_image,gray_image, Size(), 20, 20);
+	imshow(image_titles[0], image);
+	imshow(image_titles[1], gray_image);
 
 	waitKey(0);
 	return 0;
