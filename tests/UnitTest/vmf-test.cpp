00691a3507f49ccb71e17e9316f470f7370ef7e4
Mark <mpietryga93@gmail.com>
2017-04-19 10:24:26 +0200

    Improve unit test

--- tests/UnitTest/vmf-test.cpp
+++ tests/UnitTest/vmf-test.cpp
@@ -23,13 +23,13 @@ namespace UnitTestSampleVMF
 				41, 42, 43, 44, 45,
 				51, 52, 53, 54, 55);
 
-			unsigned int expected_window[4] = { 44, 54, 45, 55 };
+			int expected_window[4] = { 44, 54, 45, 55 };
 			const int window_size = 2;
-			
+
 			unsigned char *p = (unsigned char*)(example_image_5by5.data);
 			const int c = example_image_5by5.channels();
-			
-			unsigned int* window = new unsigned int[window_size*window_size*c];
+
+			int* window = new int[window_size*window_size*c];
 
 			vmf::calcPixelsWindow(p, window, example_image_5by5.rows, example_image_5by5.cols, c, window_size);
 			for (int i = 0; i < window_size*window_size*c; i++) {
@@ -37,7 +37,53 @@ namespace UnitTestSampleVMF
 			}
 
 			delete[] window;
-			delete[] expected_window;
+		}
+
+		TEST_METHOD(TestCalcPixelsWindow_Window2by2InMatrix2by5_ExpectedWindow)
+		{
+			Mat example_image_2by5 = (Mat_<unsigned char>(2, 5) <<
+				11, 12, 13, 14, 15,
+				21, 22, 23, 24, 25);
+
+			int expected_window[4] = { 14, 24, 15 ,25 };
+			const int window_size = 2;
+
+			unsigned char *p = (unsigned char*)(example_image_2by5.data);
+			const int c = example_image_2by5.channels();
+
+			int* window = new int[window_size*window_size*c];
+
+			vmf::calcPixelsWindow(p, window, example_image_2by5.rows, example_image_2by5.cols, c, window_size);
+			for (int i = 0; i < window_size*window_size*c; i++) {
+				Assert::AreEqual(expected_window[i], window[i]);
+			}
+
+			delete[] window;
+		}
+
+		TEST_METHOD(TestCalcPixelsWindow_Window2by2InMatrix5by2_ExpectedWindow)
+		{
+			Mat example_image_5by2 = (Mat_<unsigned char>(5, 2) <<
+				11, 12,
+				21, 22,
+				31, 32,
+				41, 42,
+				51, 52);
+
+			int expected_window[4] = { 41, 51, 42, 52 };
+			const int window_size = 2;
+
+			unsigned char *p = (unsigned char*)(example_image_5by2.data);
+			const int c = example_image_5by2.channels();
+
+			int* window = new int[window_size*window_size*c];
+
+			vmf::calcPixelsWindow(p, window, example_image_5by2.rows, example_image_5by2.cols, c, window_size);
+			for (int i = 0; i < window_size*window_size*c; i++) {
+				Assert::AreEqual(expected_window[i], window[i]);
+			}
+
+			delete[] window;
 		}
 
 		TEST_METHOD(TestCalcPixelsWindow_Window4by4InMatrix3by3_ReturnMinus1)
@@ -51,7 +97,7 @@ namespace UnitTestSampleVMF
 			unsigned char *p = (unsigned char*)(example_image_3by3.data);
 			const int c = example_image_3by3.channels();
 
-			unsigned int* window = new unsigned int[window_size*window_size*c];
+			int* window = new int[window_size*window_size*c];
 
 			int result = vmf::calcPixelsWindow(p, window, example_image_3by3.rows, example_image_3by3.cols, c, window_size);
 			Assert::AreEqual(-1, result);
@@ -70,7 +116,7 @@ namespace UnitTestSampleVMF
 			unsigned char *p = (unsigned char*)(example_image_3by3.data);
 			const int c = example_image_3by3.channels();
 
-			unsigned int* window = new unsigned int[window_size*window_size*c];
+			int* window = new int[window_size*window_size*c];
 
 			int result = vmf::calcPixelsWindow(p, window, example_image_3by3.rows, example_image_3by3.cols, c, window_size);
 			Assert::AreEqual(-1, result);
@@ -81,16 +127,16 @@ namespace UnitTestSampleVMF
 		TEST_METHOD(TestCalcPixelsWindow_Window2by2InMatrix2by2_ExpectedWindow)
 		{
 			Mat example_image_2by2 = (Mat_<unsigned char>(2, 2) <<
-				11, 12, 
+				11, 12,
 				21, 22);
 
-			unsigned int expected_window[4] = { 11, 21, 12, 22 };
+			int expected_window[4] = { 11, 21, 12, 22 };
 			const int window_size = 2;
 
 			unsigned char *p = (unsigned char*)(example_image_2by2.data);
 			const int c = example_image_2by2.channels();
 
-			unsigned int* window = new unsigned int[window_size*window_size*c];
+			int* window = new int[window_size*window_size*c];
 
 			vmf::calcPixelsWindow(p, window, example_image_2by2.rows, example_image_2by2.cols, c, window_size);
 			for (int i = 0; i < window_size*window_size*c; i++) {
@@ -98,7 +144,6 @@ namespace UnitTestSampleVMF
 			}
 
 			delete[] window;
-			delete[] expected_window;
 		}
 
 		TEST_METHOD(TestCalcPixelsWindow_Window1by1InMatrix2by2_ExpectedWindow)
@@ -107,13 +152,13 @@ namespace UnitTestSampleVMF
 				11, 12,
 				21, 22);
 
-			unsigned int expected_window[1] = { 22 };
+			int expected_window[1] = { 22 };
 			const int window_size = 1;
 
 			unsigned char *p = (unsigned char*)(example_image_2by2.data);
 			const int c = example_image_2by2.channels();
 
-			unsigned int* window = new unsigned int[window_size*window_size*c];
+			int* window = new int[window_size*window_size*c];
 
 			vmf::calcPixelsWindow(p, window, example_image_2by2.rows, example_image_2by2.cols, c, window_size);
 			for (int i = 0; i < window_size*window_size*c; i++) {
@@ -121,20 +166,19 @@ namespace UnitTestSampleVMF
 			}
 
 			delete[] window;
-			delete[] expected_window;
 		}
 
 		TEST_METHOD(TestCalcPixelsWindow_Window2by2InRgbMatrix2by2_ExpectedWindow)
 		{
 			Mat example_rgb_image_2by2(2, 2, CV_8UC3, Scalar(1, 100, 255));
 
-			unsigned int expected_window[4*3] = { 1, 100, 255, 1, 100, 255, 1, 100, 255, 1, 100, 255 };
+			int expected_window[4 * 3] = { 1, 100, 255, 1, 100, 255, 1, 100, 255, 1, 100, 255 };
 			const int window_size = 2;
 
 			unsigned char *p = (unsigned char*)(example_rgb_image_2by2.data);
 			const int c = example_rgb_image_2by2.channels();
 
-			unsigned int* window = new unsigned int[window_size*window_size*c];
+			int* window = new int[window_size*window_size*c];
 
 			vmf::calcPixelsWindow(p, window, example_rgb_image_2by2.rows, example_rgb_image_2by2.cols, c, window_size);
 			for (int i = 0; i < window_size*window_size*c; i++) {
@@ -142,7 +186,6 @@ namespace UnitTestSampleVMF
 			}
 
 			delete[] window;
-			delete[] expected_window;
 		}
 
 	};
