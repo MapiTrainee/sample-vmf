64e598e77e6c2475dcfd82b785479b7f0e43347c
Mark <mpietryga93@gmail.com>
2017-04-20 11:19:23 +0200

    Improve entry point, project properties

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,43 @@
 
 #include <iostream>
 #include <string>
+#include "vmf.h"
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv)
 {
-	string image_name = "img\\test.png";
+	string image_name = "img\\sample.png";
 	cout << image_name << endl;
 
 	Mat image, gray_image;
 	image = imread(image_name, IMREAD_COLOR);
-	cvtColor(image, gray_image, CV_BGR2GRAY);
-	cout << image << endl << endl;
-	cout << gray_image << endl;
-
 	if (image.empty())
 	{
 		cout << "Could not open or find the image" << endl;
 		return -1;
 	}
+	cvtColor(image, gray_image, CV_BGR2GRAY);
 
-	string image_titles[] = { "Plain image", "Gray image" };
-	namedWindow(image_titles[0], CV_WINDOW_AUTOSIZE);
-	namedWindow(image_titles[1], CV_WINDOW_AUTOSIZE);
-	//for better preview test.png
-	resize(image, image, Size(), 20, 20);
-	resize(gray_image,gray_image, Size(), 20, 20);
+	vector<string> image_titles = { "Plain image", "Gray image", "Filtered color image", "Filtered gray image" };
+	for (int i = 0; i < image_titles.size(); i++) {
+		namedWindow(image_titles[i], CV_WINDOW_AUTOSIZE);
+	}
+
 	imshow(image_titles[0], image);
 	imshow(image_titles[1], gray_image);
+
+	Mat filtered_image,
+		filtered_gray_image;
+
+	int window_side = 3;
+
+	vmf::vmfFilter(image, filtered_image, window_side);
+	vmf::vmfFilter(gray_image, filtered_gray_image, window_side);
+
+	imshow(image_titles[2], filtered_image);
+	imshow(image_titles[3], filtered_gray_image);
 
 	waitKey(0);
 	return 0;
