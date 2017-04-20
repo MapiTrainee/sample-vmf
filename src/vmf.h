fb7a232f64c0e0620be6ba9bc2064053c79d2c13
Mark <mpietryga93@gmail.com>
2017-04-20 11:16:21 +0200

    Implement vmf filter

--- src/vmf.h
+++ src/vmf.h
@@ -11,7 +11,7 @@ namespace vmf {
 
 	void setPixel(unsigned char* p, int* pixel, int c) {
 		int* pp = pixel; //pixel pointer
-		
+
 		for (int i = 0; i < c; i++) {
 			*p = *pp;
 			pp++;
@@ -19,16 +19,16 @@ namespace vmf {
 		}
 	}
 
-	void findPixel(int* window, int* pixel, int index, int n, int c) {
+	void findPixel(int* window, int* pixel, int index, int c) {
 		int* pp = pixel; //pixel pointer
-		
+
 		for (int i = 0; i < c; i++) {
-			*pp = window[index*c];
+			*pp = window[index*c + i];
 			pp++;
 		}
 	}
 
-	int findMinIndex(double* sums, int n) {
+	int findIndexOfMinValue(double* sums, int n) {
 		double *p = sums;
 		double min = *p;
 		int index = 0;
@@ -46,23 +46,21 @@ namespace vmf {
 	}
 
 	void sumMatrixRows(double* matrix, double* sums, int rows, int cols) {
-		double *p = matrix;
+		double *mp = matrix;
 		double *sp = sums;
 
 		for (int i = 0; i < rows; i++) {
 			double sum = 0;
 			for (int j = 0; j < cols; j++) {
-				cout << *p << " ";
-				sum += *p;
-				p++;
+				sum += *mp;
+				mp++;
 			}
 			*sp = sum;
 			sp++;
 		}
 	}
 
-	int calcDistanceMatrix(int* window, double* distance_matrix, int n, int c) {
-		if (n < 1) return -1;
+	void calcDistanceMatrixFromWindow(int* window, double* distance_matrix, int n, int c) {
 
 		double *rp, *cp;
 		rp = distance_matrix; //distance matrix rows pointer
@@ -96,73 +94,77 @@ namespace vmf {
 			wp = &window[(i + 1)*c];
 			cp = &distance_matrix[i + 1];
 		}
-		return 1;
+
 	}
 
-	void createDistanceMatrix(int* window, int size, int channels) {
-		double *distance_matrix = new double[size*size]();
-
-		calcDistanceMatrix(window, distance_matrix, size, channels);
-		for (int i = 1; i <= size*size; i++) {
-			cout << distance_matrix[i - 1] << " ";
-			if (i % size == 0) cout << endl;
-		}
-
-		delete[] distance_matrix;
-	}
-
-	int calcPixelsWindow(unsigned char *p, int *window, int rows, int cols, int c, int n) {
-		if (rows < n || cols < n || n < 1) {
+	int calcPixelsWindowAndFilterPixels(unsigned char *p, int *window, int rows, int cols, int c, int n) {
+		if (rows < n || cols < n || c > 3 || n < 1) {
 			return -1;
 		}
 		int *wp; //window pointer
 		wp = window;
 		unsigned char *midp; //middle point of window
 		midp = p;
+		int window_size = n*n;
+
+		double *distance_matrix = new double[window_size*window_size]();
+		double *sums = new double[window_size];
+		int pixel[3] = { 0 };
 
 		for (int i = 0; i < rows - (n - 1); i++) {
 			for (int j = 0; j < cols - (n - 1); j++) {
 				for (int k = 0; k < n; k++) {
 					for (int l = 0; l < n; l++) {
+						if (k == n / 2 && k == l) {
+							midp = p;
+						}
 						for (int m = 0; m < c; m++) {
-							//cout << (int)*p << " ";
-							if (k == n / 2 && k == l) {
-								// << (int)*p << " ";
-								midp = p;
-							}
 							*wp = (int)*p;
 							wp++;
 							p++; //grayscale or rgb values
 						}
+
 						p = p - c; //return after above changes
 						p = p + cols * c;  // rows of window
 					}
 					p = p - n * cols * c + c; // columns of window
 				}
+				calcDistanceMatrixFromWindow(window, distance_matrix, window_size, c);
+				sumMatrixRows(distance_matrix, sums, window_size, window_size);
+				int index = findIndexOfMinValue(sums, window_size);
+				findPixel(window, pixel, index, c);
+				setPixel(midp, pixel, c);
+
 				wp = &window[0];
 				p = p - (n - 1) * c; // columns of matrix
 			}
 			p = p + (n - 1) * c; // rows of matrix
 		}
+
+		delete[] sums;
+		delete[] distance_matrix;
 		return 1;
 	}
 
-	void createPixelsWindow(Mat image, unsigned int size) {
+	void vmfFilter(Mat& src, Mat& dest, int window_side) {
 
-		unsigned char *p = (unsigned char*)(image.data);
-		const int c = image.channels();
-		const int n = size;
-		int* window = new int[n*n*c];
-		calcPixelsWindow(p, window, image.rows, image.cols, c, n);
-		for (int a = 0; a < n*n*c; a++) {
-			cout << window[a] << " ";
-		}
+		int pixel_border = window_side / 2;
+
+		copyMakeBorder(src, dest, pixel_border, pixel_border, 
+			pixel_border, pixel_border, BORDER_REPLICATE);
+
+		unsigned char *p = (unsigned char*)(dest.data);
+		int c = dest.channels();
+		int cols = dest.cols;
+		int rows = dest.rows;
+
+		int* window = new int[window_side*window_side*c];
+		calcPixelsWindowAndFilterPixels(p, window, rows, cols, c, window_side);
 		delete[] window;
-	}
 
+		dest = dest(Rect(pixel_border, pixel_border,
+			cols - 2 * pixel_border, rows - 2 * pixel_border));
 
-	int vmfFilter(Mat image) {
-		return -1;
 	}
 
 }
